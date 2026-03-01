6c77ec7a3a63627073ac3e515b55bf1d04e3b544
kalaividhyasribsccs2425-del <kalaividhyasribsc.cs2425@gmail.com>
2026-03-01 17:56:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
-const apiKey = "9f3a8c7b6d5e4f1234567890abcd1234";
 async function getWeather() {
+    const city = document.getElementById("city").value;
+    const apiKey = "YOUR_API_KEY_HERE";
+    const url = https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric;
 
-    const city = document.getElementById("cityInput").value.trim();
+    const response = await fetch(url);
+    const data = await response.json();
 
-    if (!city) {
-        alert("Please enter a city name");
-        return;
-    }
-
-    const url = http://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_KEY&units=metric
-
-    try {
-
-        const response = await fetch(url);
-
-        const data = await response.json();
-
-        if (data.cod != 200) {
-            alert(data.message);
-            return;
-        }
-
-        document.getElementById("cityName").innerText = data.name;
-        document.getElementById("temperature").innerText =
-            "Temperature: " + data.main.temp + " °C";
-        document.getElementById("description").innerText =
-            "Condition: " + data.weather[0].description;
-        document.getElementById("humidity").innerText =
-            "Humidity: " + data.main.humidity + "%";
-        document.getElementById("windSpeed").innerText =
-            "Wind Speed: " + data.wind.speed + " km/h";
-
-    } catch (error) {
-        alert("Error fetching data. Check console.");
-        console.log(error);
-    }
-}
+    document.getElementById("result").innerHTML =
+        `<h3>${data.name}</h3>
+         <p>Temperature: ${data.main.temp} °C</p>
+         <p>Weather: ${data.weather[0].description}</p>`;
+} 
